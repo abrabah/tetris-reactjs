4bb92e13f27e4d579480489cae17ed1df1293ead
Abraham Bah <abraham.bah@skatteetaten.no>
2015-11-03 15:51:42 +0100

    added counter.jsx

---
+++ js/Counter.jsx
@@ -0,0 +1,17 @@
+
+
+import React from 'react' 
+import ReactDom from 'react-dom'
+
+const Counter = React.createClass({
+
+
+	/*TODO 
+	 * Listen for store changes
+	 * when store changes, start incrementing the view
+	 * */
+
+
+})
+
+export default Counter
