5e174bb51e139d296d2f55f17e518c028c50a7de
Abraham Bah <abraham.bah@bekk.no>
2015-11-07 16:53:59 +0100

    score view updates in a nice smooth way.

--- js/CounterView.jsx
+++ js/CounterView.jsx
@@ -5,17 +5,36 @@ import Store from 'TetrisStore.jsx'
 const Counter = React.createClass({
 
 
-    componentDidMount: function() {
+    componentDidMount: function () {
         Store.on(this.props.name.toLowerCase() + 'Change', this._changeValue)
     },
 
 
-    getInitialState: function(){
-        return {value:Store.get(this.props.name.toLowerCase())}
+    getInitialState: function () {
+        return {value: Store.get(this.props.name.toLowerCase())}
     },
 
-    _changeValue: function(newValue) {
-        this.setState({value:newValue})
+    incrementValue: function (newValue) {
+
+        const tmpValue = Math.ceil(newValue * (-Math.pow(2, -10 * this.state.tc / 150) + 1))
+
+
+        if (Math.abs(tmpValue - newValue) < 2 || this.state.tc > 150) {
+            clearInterval(this.incrementValueInterval)
+            this.setState({value: newValue})
+        } else
+            this.setState({value: tmpValue, tc: this.state.tc + 1})
+    },
+
+
+    _changeValue: function (newValue) {
+        const ths = this
+        clearInterval(this.incrementValueInterval)
+
+        if (Math.abs(newValue - this.state.value) < 2)
+            this.setState({value: newValue, tc: 0})
+        else
+            this.incrementValueInterval = setInterval((evt) => ths.incrementValue(newValue), 10)
     },
 
     render: function () {
